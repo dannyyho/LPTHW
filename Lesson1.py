ae8531deb8ea14081ce93fabb0349414a2be8007
Danny Ho <41646192+dannyyho@users.noreply.github.com>
2023-01-09 19:16:42 -0500

    Update Lesson1.py

--- Lesson1.py
+++ Lesson1.py
@@ -1,5 +1,104 @@
-# this is where it starts
+from sys import argv
+
+script, user_name = argv
+prompt = '> '
+
+print(f"Hi {user_name}, I'm the {script} script.")
+print("I'd like to ask you a few questions.")
+print(f"Do you like me {user_name}?")
+likes = input(prompt)
+
+print(f"Where do you live {user_name}?")
+lives = input(prompt)
+
+print("What kind of computer do you have?")
+computer = input(prompt)
+
+print(f"""
+Alright, so you said {likes} about liking me.
+You live in {lives}. Not sure where that is.
+And you have a {computer} computer. Nice.
+""")
+
+
+
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print(f"Here's your file {filename}:")
+print(txt.read())
+
+print("Type the filename again:")
+file_again = input("> ")
+
+txt_again = open(file_again)
+
+print(txt_again.read())
+
+
+from sys import argv
+
+script, filename = argv
+
+print(f"We're going to erase {filename}.")
+print("If you don't want that, hit CTRL-C (^C).")
+print("If you do want that, hit RETURN.")
+
+input("?")
+
+print("Opening the file...")
+target = open(filename, 'w')
+
+DING AND WRITING FILES 85
+print("Truncating the file. Goodbye!")
+target.truncate()
+
+print("Now I'm going to ask you for three lines.")
+
+line1 = input("line 1: ")
+line2 = input("line 2: ")
+line3 = input("line 3: ")
+
+print("I'm going to write these to the file.")
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+print("And finally, we close it.")
+target.close()
 
 
 
 
+# this one is like your scripts with argv
+def print_two(*args):
+arg1, arg2 = args
+print(f"arg1: {arg1}, arg2: {arg2}")
+
+# ok, that *args is actually pointless, we can just do this
+def print_two_again(arg1, arg2):
+print(f"arg1: {arg1}, arg2: {arg2}")
+
+# this just takes one argument
+def print_one(arg1):
+print(f"arg1: {arg1}")
+
+# this one takes no arguments
+def print_none():
+print("I got nothin'.")
+
+
+print_two("Zed","Shaw")
+print_two_again("Zed","Shaw")
+print_one("First!")
+print_none()
+
+
+## Functions and Files
